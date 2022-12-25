e887ae5f0ab2c14188c8ee15748a53d6595e0441
VladyslavLiapko <109310520+VladyslavLiapko@users.noreply.github.com>
2022-12-25 19:39:48 +0200

    add file color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  } 
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    if (timerId !== true) {
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+      }
+  });
+  
+  
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+  if (timerId !== true) {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+  });
